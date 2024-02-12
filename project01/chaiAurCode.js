1dd5cc100bd5bde84bdca4b222b6758d3900cdcf
Deependra Kashyap <140043156+codebydkashyap@users.noreply.github.com>
2024-02-12 23:05:03 +0530

    colorSchemeChanger

--- project01/chaiAurCode.js
+++ project01/chaiAurCode.js
@@ -3,27 +3,31 @@ const body = document.querySelector('body')
 button.forEach((button)=>{
 console.log(button);
 button.addEventListener('click',function(e){
-    console.log(e);
     console.log(e.target.id);
     switch (e.target.id) {
         case 'grey':
             body.style.backgroundColor="grey"
             body.style.color="white"
             break;
+            
         case 'white':
              body.style.backgroundColor="white"
              body.style.color="black"
             break;
+            
         case 'blue':
             body.style.backgroundColor="#8a9cd7"
             body.style.color="black"
             break;
+            
         case 'yellow':
             body.style.backgroundColor="yellow"
             body.style.color="green"
             break;
     
         default:
+            body.style.backgroundColor="white"
+            body.style.color="black"
             break;
     }
 })
